03c2b8eb63874862bae5d7b96b8563cc47db1843
sabaatharique <saba.5.9.2003@gmail.com>
2024-08-11 00:50:37 +0400

    entry deletion works

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# IOU
+# IOU [(live)](https://iou-six.vercel.app/)
 A simple web app using JavaScript, HTML and CSS to track daily debt.
--- app.js
+++ app.js
@@ -38,21 +38,56 @@ const addNewLog = () => {
 }
 
 const displayEntries = () => {
+    output.innerHTML = '';
     for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
-    
-        output.innerHTML += `<div class="entry-card">
-                                <div class="name">
-                                    <h3>${key}</h3>
-                                    <button class="add-btn" type="button">ADD</button>
-                                </div>
-                                <div class="details"> 
-                                    <p>${value}</p>
-                                    <button class="del-btn" type="button">DELETE</button>
-                                </div>
-                            </div>`;
+        const entryName = localStorage.key(i);
+        const entryLogs = retrievefromLocalStorage(entryName);
+
+        const entryCard = document.createElement('div');
+        entryCard.className = 'entry-card';
+
+        const details = document.createElement('div');
+        details.className = 'details';
+        details.innerHTML = `<h3>${entryName}</h3>
+                            <button class="del-btn" id="${i}" type="button">DELETE</button>`;
+
+        const allLogs = document.createElement('div');
+        allLogs.className = 'logs';
+        entryLogs.forEach((log, j) => {
+            const eachLog = document.createElement('div');
+            eachLog.className = 'log';
+            eachLog.innerHTML = `<p> Date: ${log.date} | Owes you: ${log.owesYou} | You Owe: ${log.youOwe} | Note: ${log.note}</p>
+            <button class="clr-btn" data-entry="${entryName}" data-index="${j}" type="button">CLEAR</button>`;
+            allLogs.appendChild(eachLog);
+        });
+
+        entryCard.appendChild(details);
+        entryCard.appendChild(allLogs);
+        output.appendChild(entryCard);
     }
+
+    const clrBtn = document.querySelectorAll('.clr-btn').forEach(button => {
+        button.addEventListener('click', function() {
+            const name = this.dataset.entry;
+            const logIndex = this.dataset.index;
+            let logs = retrievefromLocalStorage(name);
+            logs.splice(logIndex, 1);
+            if (logs.length > 0) {
+                localStorage.setItem(name, JSON.stringify(logs)); 
+            } else {
+                localStorage.removeItem(name);
+            }           
+            displayEntries();
+        });
+    });
+
+    const delBtn = document.querySelectorAll('.del-btn').forEach(button => {
+        button.addEventListener('click', function() {
+            const name = localStorage.key(this.id);
+            localStorage.removeItem(name);
+            displayEntries();
+        });
+    });
 }
 
 newBtn.onclick = addNewLog;
