ca452ddbe3270567419854553c11b530c1b4992f
sabaatharique <saba.5.9.2003@gmail.com>
2024-08-11 18:05:58 +0400

    working navbar

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const inpOwesYou = document.getElementById('owed-to-you');
 const inpDate = document.getElementById('date');
 const inpNote = document.getElementById('note');
 
+const delAllBtn = document.getElementById('del-all');
+
+const clearAll = () => {
+    alert("All entries will be cleared.");
+    localStorage.clear();
+    displayEntries();
+}
+
 const retrievefromLocalStorage = (name) => {
     const logs = localStorage.getItem(name);
     if (logs == null)
@@ -84,9 +92,9 @@ const displayEntries = () => {
         details.className = 'details';
         details.innerHTML = `<h3>${entryName} : ${who} owe ${Math.abs(totalOwed)}</h3>
                             <button class="del-btn" id="${i}">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                                <path fill="currentColor"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                    <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                    <path d="M576 128c0-35.3-28.7-64-64-64L205.3 64c-17 0-33.3 6.7-45.3 18.7L9.4 233.4c-6 6-9.4 14.1-9.4 22.6s3.4 16.6 9.4 22.6L160 429.3c12 12 28.3 18.7 45.3 18.7L512 448c35.3 0 64-28.7 64-64l0-256zM271 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
                                 </svg>
                             </button>`;
 
@@ -98,6 +106,7 @@ const displayEntries = () => {
             eachLog.innerHTML = `<p> Date: ${log.date} | You Owe: ${log.youOwe} | Owes you: ${log.owesYou} | Note: ${log.note}</p>
             <button class="clr-btn" data-entry="${entryName}" data-index="${j}">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                    <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                     <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
                 </svg>
             </button>`;
@@ -126,6 +135,8 @@ const displayEntries = () => {
 }
 
 newBtn.onclick = addNewLog;
+
+delAllBtn.onclick = clearAll;
 
 displayEntries();
 
